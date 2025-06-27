0d5ec7d6463e07bc53121e8bc86b656b9a688fd6
luna-77 <zlunawang@gmail.com>
2025-06-27 15:51:56 -0700

    Make tinted body with opaque edges

--- trash_bin.py
+++ trash_bin.py
@@ -3,21 +3,28 @@ import time
 
 p.connect(p.GUI)
 
-bin_recycling = p.loadURDF("trash_bin.urdf", basePosition=[0, 0, 0],)
+bin_recycling = p.loadURDF("trash_bin.urdf", basePosition=[0, 0, 0])
 bin_trash = p.loadURDF("trash_bin.urdf", basePosition=[1, 0, 0])
 
-
-
-def set_color(body_id, color):
+def set_tinted_body_with_opaque_edges(body_id, body_color, edge_color):
     visual_shapes = p.getVisualShapeData(body_id)
     for shape in visual_shapes:
         link_index = shape[1]
-        p.changeVisualShape(body_id, link_index, rgbaColor=color)
+        shape_name = shape[4].decode("utf-8") if isinstance(shape[4], bytes) else shape[4]
 
-blue = [0, 0, 1, 1]
-black = [0, 0, 0, 1]
-set_color(bin_recycling, blue)
-set_color(bin_trash, black)
+        if "edge" in shape_name.lower() or "rim" in shape_name.lower() or link_index == 1:
+            p.changeVisualShape(body_id, link_index, rgbaColor=edge_color)
+        else:
+            p.changeVisualShape(body_id, link_index, rgbaColor=body_color)
+
+blue_body = [0, 0, 1, 0.5]
+blue_edge = [0, 0, 1, 1.0]
+
+black_body = [0.1, 0.1, 0.1, 0.5]
+black_edge = [0, 0, 0, 1.0]
+
+set_tinted_body_with_opaque_edges(bin_recycling, blue_body, blue_edge)
+set_tinted_body_with_opaque_edges(bin_trash, black_body, black_edge)
 
 while True:
     p.stepSimulation()
